8bea6c64b191e97c86f2f111300111260b8f074f
TupotaArtem <tupotaart@gmail.com>
2025-03-30 19:13:10 +0300

    commit_2

--- task_2.py
+++ task_2.py
@@ -1,18 +1,13 @@
 import random
 
 def get_numbers_ticket(min_value, max_value, quantity):
-  
-    if not (1 <= min_value <= max_value <= 1000) or not (min_value <= quantity <= max_value):
+
+    if not (1 <= min_value <= max_value <= 1000) or not (1 <= quantity <= (max_value - min_value + 1)):
         return []
-    
-    numbers = random.sample(range(min_value, max_value + 1), quantity)    
-   
+
+    numbers = random.sample(range(min_value, max_value + 1), quantity)
+
     return sorted(numbers)
 
-def main():
-    lottery_numbers = get_numbers_ticket(1, 49, 6)
-    print("Ваші лотерейні числа:", lottery_numbers)
-
-
-if __name__=="__main__":
-    main()
+lottery_numbers = get_numbers_ticket(1, 49, 6)
+print("Ваші лотерейні числа:", lottery_numbers)
